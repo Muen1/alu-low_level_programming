6544f759b1b6c052ab804b4ad305eb64bed3a2a2
Cynthia Mutie <c.mutie@alustudent.com>
2026-01-29 05:19:45 +0200

    functions nested loops

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: number to compute
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+    return (n < 0 ? -n : n);
+}
